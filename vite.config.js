f55e3e460951bb8d3cbbcbb5efa5d56f7cbad2d2
HugoARV <162652209+hugoandresriosvega@users.noreply.github.com>
2024-04-25 10:39:10 -0500

    agregar

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://hugoandresriosvega.github.io/client"
+  base: "https://hugoandresriosvega.github.io/BDGesticol"
 })
